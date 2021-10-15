a1e93161467857f17c5affbab18ebbe9cf19d11a
BreeCoder <BreeCoder@users.noreply.github.com>
2021-10-14 22:01:13 -0500

    prolog
    updated the functions for prolog section

--- covid-es-api/Api/Application/Prolog_KnowledgeBase/patientdiagnosis.pl
+++ covid-es-api/Api/Application/Prolog_KnowledgeBase/patientdiagnosis.pl
@@ -5,3 +5,10 @@ covert(temp).
 convert(Temp):-write('Enter the temperature'),nl,read(Temp),
    Newtemp is Temp+32,
     write(Newtemp).
+
+/*
+This is to check if the reading shows that the blood pressure is high
+0 = yes
+1 = no
+*/
+blood_presure_check(Systolic,Diastolic):-Systolic=<140,Diastolic=<90->Answer is 0.
