bf4cf3ad31ba2c43b9c8a8cc7efd8d815918ae80
BreeCoder <BreeCoder@users.noreply.github.com>
2021-10-17 18:50:46 -0500

    ..

--- covid-es-api/Api/patient_diagnosis.pl
+++ covid-es-api/Api/patient_diagnosis.pl
@@ -15,3 +15,10 @@ This is to check if the reading shows that the blood pressure is high
 1 = no
 */
 blood_presure_check(Systolic,Diastolic):-Systolic=<140,Diastolic=<90->Answer is 0.
+
+
+alert_spike(Ans):-(Ans==y)->CovidNum is NewCovidNum+1,
+    actions(Ans);write('Okay').
+
+actions(Ans):-write('The short term actions are as follows: '),
+    write('The long term actions are as follows:').
