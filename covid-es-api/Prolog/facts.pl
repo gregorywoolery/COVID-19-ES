2894778cde86da5385ec8759ec2aafed833e50ff
Gregory Woolery <57403853+gregorywoolery@users.noreply.github.com>
2021-11-15 19:59:23 -0500

    Update facts.pl

--- covid-es-api/Prolog/facts.pl
+++ covid-es-api/Prolog/facts.pl
@@ -53,4 +53,3 @@ symptoms_type_variant(mild, delta, "sore throat").
 symptoms_type_variant(severe, mu, "blurred vision").
 symptoms_type_variant(mild, mu, "diarrhea").
 symptoms_type_variant(severe, mu, "loss of taste and smell").
-long_term_actions("Keep a 6ft distance when outside.").
